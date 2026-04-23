97afdc1051d33d6c365a363ec7a1142559292af6
Rishabh Gupta <rishabhgupta999175@gmail.com>
2026-04-23 23:35:36 +0530

    Time: 25 ms (30.15%) | Memory: 17.3 MB (82.73%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -7,11 +7,11 @@ public:
         sort(nums.begin(), nums.end());
         
         for (int i = 0; i < n - 3; i++) {
-            // Skip duplicates for i
+            
             if (i > 0 && nums[i] == nums[i - 1]) continue;
             
             for (int j = i + 1; j < n - 2; j++) {
-                // Skip duplicates for j
+                
                 if (j > i + 1 && nums[j] == nums[j - 1]) continue;
                 
                 int left = j + 1;
@@ -23,7 +23,7 @@ public:
                     if (sum == target) {
                         ans.push_back({nums[i], nums[j], nums[left], nums[right]});
                         
-                        // Skip duplicates
+                
                         while (left < right && nums[left] == nums[left + 1]) left++;
                         while (left < right && nums[right] == nums[right - 1]) right--;
                         
