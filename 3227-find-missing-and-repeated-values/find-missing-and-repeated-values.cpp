4420d519ff3c3d6ae93c69336321d325d9a66440
Rishabh Gupta <rishabhgupta999175@gmail.com>
2026-06-27 01:47:58 +0530

    Time: 19 ms (22.67%) | Memory: 31.7 MB (35.53%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
@@ -1,34 +1,26 @@
 class Solution {
 public:
     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
+        unordered_set<int>s;
+        int n=grid.size();
+        int duplicate=0;
+        int actual_sum=0;
+        int total= n*n;
 
-        unordered_set<int> s;
-
-        int n = grid.size();
-        int total = n * n;
-
-        int expected_sum = total * (total + 1) / 2;
-        int actual_sum = 0;
-
-        int duplicate = 0;
-
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-
-                int a = grid[i][j];
-                actual_sum += a;
-
-                if(s.find(a) == s.end()) {
-                    s.insert(a);
+        int expected_sum= total*(total+1)/2;
+        for(int i=0; i<n;i++){
+            for(int j=0;j<n;j++){
+                int a =grid[i][j];
+                actual_sum+=a;
+                if(s.find(a)==s.end()){
+                   s.insert(a);
                 }
                 else {
-                    duplicate = a;
+                    duplicate=a;
                 }
             }
         }
-
-        int missing = expected_sum - actual_sum + duplicate;
-
-        return {duplicate, missing};
+        int missing_element=expected_sum-actual_sum + duplicate;
+        return{duplicate,missing_element};
     }
 };
